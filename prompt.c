766d810cfdfb7127e1239dc9bc89d198e927f2b7
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-08-20 14:25:18 +0000

    betty fixed

--- prompt.c
+++ prompt.c
@@ -1,21 +1,14 @@
 #include "s_shell.h"
 
 /**
-* _prompt - display a $ and wait for user input
+* _prompt - display a prompt and wait for user input
 * Return: Void
 */
 
 
 void _prompt(void)
 {
-	char *buffer;
-	size_t buffersize = 32;
 
-	buffer = malloc(buffersize * sizeof(char));
-
-	if (buffer)
-	{
-	write(STDOUT_FILENO, "$>", 3); /*printf"$ ");*/
-	}
-	free(buffer);
+	if (isatty(STDIN_FILENO))
+		write(1, "KBS$> ", 6); /*printf"$ ");*/
 }
