f4ea1f4ed98b366ab939d4bcb12cd19608914304
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-08-24 14:02:33 +0000

    prompt and aux modified

--- prompt.c
+++ prompt.c
@@ -29,39 +29,3 @@ char *prompt(void)
 	}
 	perror("Error");
 }
-/**
- * fndpath - Find the path for a specific command
- * @name: Command
- * Return: Path name ending in command
- */
-char *fndpath(char *name)
-{
-	const char *path = "PATH";
-	char *value, *tok;
-	char *dir[8];
-	struct stat statbuff;
-	int num = 0;
-
-	value = _getenv(path);
-	tok = strtok(value, TOK_DELIM);
-	do {
-		dir[num] = NULL;
-		dir[num] = _strdup(tok);
-		dir[num] = str_concat(dir[num], name);
-		if (name[0] == '/')
-			break;
-		if (_strcmp(name, "./") == 0)
-			break;
-		if (stat(dir[num], &statbuff) == 0)
-		{
-			free(value);
-			return (dir[num]);
-		}
-		else
-			num++;
-		tok = strtok(NULL, TOK_DELIM);
-	} while (tok != NULL);
-	free(dir[num]);
-	free(value);
-	return (name);
-}
