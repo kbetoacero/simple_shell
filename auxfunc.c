f21e944f9ef9f412b1e3e9526f45958838a4de52
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-08-24 14:06:12 +0000

    helper and aux modified

--- auxfunc.c
+++ auxfunc.c
@@ -93,39 +93,3 @@ int _strcmp(char *s1, char *s2)
 	else
 		return (0);
 }
-/**
- * fndpath - Find the path for a specific command
- * @name: Command
- * Return: Path name ending in command
- */
-char *fndpath(char *name)
-{
-        const char *path = "PATH";
-        char *value, *tok;
-        char *dir[8];
-        struct stat statbuff;
-        int num = 0;
-
-        value = _getenv(path);
-        tok = strtok(value, TOK_DELIM);
-        do {
-                dir[num] = NULL;
-                dir[num] = _strdup(tok);
-                dir[num] = str_concat(dir[num], name);
-                if (name[0] == '/')
-                        break;
-                if (_strcmp(name, "./") == 0)
-                        break;
-                if (stat(dir[num], &statbuff) == 0)
-                {
-                        free(value);
-                        return (dir[num]);
-                }
-                else
-                        num++;
-                tok = strtok(NULL, TOK_DELIM);
-        } while (tok != NULL);
-        free(dir[num]);
-        free(value);
-        return (name);
-}
